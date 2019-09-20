412cb26493a6fb43a8dbe6ef42e2df57ac97e695
AndrewPhills <andrei97filip@gmail.com>
2019-09-20 17:21:29 +0300

    done footer and header. done forgot password incoming css for forgot password and dashboard

--- web-client/src/app/forgot-password/forgot-password.component.ts
+++ web-client/src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  email: string;
+  validationMessage: string;
+  isInvalid: boolean;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  initForm() {
+    this.form = this.fb.group({
+      email: ['',  Validators.required],
+      });
   }
 
+  ngOnInit() {
+    this.validationMessage = '';
+    this.isInvalid = false;
+    this.initForm();
+  }
+
+  doNext() {
+  debugger;
+    if (this.form.valid) {
+      if (this.email === 'abc@gmail.com') {
+        document.getElementById("demo").innerHTML = "";
+        this.router.navigate(['/login']);
+      } else {
+        document.getElementById("demo").innerHTML = "Please write a proper email that is already registered";
+        this.isInvalid = true;
+        this.validationMessage = 'Invalid login !';
+      }
+    }
+  }
 }
